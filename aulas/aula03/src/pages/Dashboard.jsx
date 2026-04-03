11c3d6e97f6ee5dc55e2dc3599721c34955d47ec
João Victor <j.victoriesb@gmail.com>
2026-04-02 21:52:31 -0300

    trabalhando componentes do react

---
+++ aulas/aula03/src/pages/Dashboard.jsx
@@ -0,0 +1,22 @@
+import Card from "../components/Card";
+import Layout from "./Layout";
+
+function Dashboard() {
+  return (
+    <>
+      <Layout />
+      <Sidebar />
+      <main>
+        <Topbar />
+        <h2>Bem-vindo ao portal do aluno</h2>
+        <section>
+          <Card />
+          <Card />
+          <Card />
+        </section>
+      </main>
+    </>
+  );
+}
+
+export default Dashboard;
