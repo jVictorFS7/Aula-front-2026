3d19f881e436469f6866f1f5e61b0c80425ad086
João Victor <j.victoriesb@gmail.com>
2026-04-16 21:49:10 -0300

    implementei estados e eventos no react

---
+++ aulas/aula03/src/components/InputSenha.jsx
@@ -0,0 +1,11 @@
+function InputSenha({erro, mudaValor}){
+    return <>
+    <label htmlFor="senha">Senha</label>
+        <input type="password" id="senha" name="senha" 
+          onChange={mudaValor}
+        />
+        <p>{erro}</p>
+    </>;
+}
+
+export default InputSenha;
